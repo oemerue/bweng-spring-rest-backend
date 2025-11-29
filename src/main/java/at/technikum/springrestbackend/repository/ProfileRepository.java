3309c969025dab815fc91ecbb0d3234580ac6457
Ömer <oe.uendemir@gmail.com>
2025-11-29 14:33:39 +0100

    create class: SecurityConfig edit class: ProfileRepository, JwtService

--- src/main/java/at/technikum/springrestbackend/repository/ProfileRepository.java
+++ src/main/java/at/technikum/springrestbackend/repository/ProfileRepository.java
@@ -2,7 +2,10 @@ package at.technikum.springrestbackend.repository;
 
 import at.technikum.springrestbackend.entity.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
+
     boolean existsByUsername(String username);
+    Optional<Profile> findByUsername(String username);
 }
