da24566c30fe0930468c6b3be8ad7819e015b79b
Lama <fhb201269@fh-vie.ac.at>
2025-11-30 20:31:31 +0100

    create classes: CustomUserDetailsService, JwtAuthenticationFilter

---
+++ src/main/java/at/technikum/springrestbackend/security/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package at.technikum.springrestbackend.security;
+
+import at.technikum.springrestbackend.entity.Profile;
+import at.technikum.springrestbackend.repository.ProfileRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final ProfileRepository profileRepository;
+
+    public CustomUserDetailsService(ProfileRepository profileRepository) {
+        this.profileRepository = profileRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Profile profile = profileRepository
+                .findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(profile.getUsername())
+                .password(profile.getPasswordHash())
+                .roles(profile.getRole()) // anpassen, falls anders
+                .build();
+    }
+}
